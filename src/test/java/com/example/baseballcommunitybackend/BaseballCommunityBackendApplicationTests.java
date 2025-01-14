2edef6f9046e31e3a2b267285bdc25ff8393cc13
jwlee <lemonade4813@gmail.com>
2025-01-14 23:26:42 +0900

    init : mongoDB 연동

---
+++ src/test/java/com/example/baseballcommunitybackend/BaseballCommunityBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baseballcommunitybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseballCommunityBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
